ea80307ab8537038724e11178e76ed6c00c24fda
FaithAdhiambo456 <156072384+FaithAdhiambo456@users.noreply.github.com>
2025-01-31 17:28:47 +0300

    Print statement, comments and concatination in Python

--- Lesson1.py
+++ Lesson1.py
@@ -9,3 +9,31 @@ var = 100
 
 print("Hello World!")
 
+#Printing two lines on the same line
+print("I'm learning Python. \
+We're starting with a print statement")
+
+#Printing multiple statements on separate lines
+
+print(""" I'm learning Python.
+ We're starting with print statements with three double quotations""")
+
+#Two separate sentences on the same line
+print("We'll first learn how to Print.", end="###" )
+print("The we'll learn how to comment code")
+
+#To print a statement 'x' number of times
+print("I want to print this statement thrice...." *3)
+
+print("." *40)
+
+#String concatination
+#1. Using plus sign (+)
+print("This is how to..." + "concatinate two or more strings..." + "in Python")  
+
+print("."* 25)
+#2 using commas(,)
+print("This is how to...", "concatinate two or more strings...", "in Python", "using commas")  
+print("."* 25)
+#Concatination of both strings and numbers
+print("Hi I have", 20, "oranges and", 30, "apples")
