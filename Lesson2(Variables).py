97f486247431b9ada57c56c6149c7082a483c8d5
FaithAdhiambo456 <156072384+FaithAdhiambo456@users.noreply.github.com>
2025-01-31 18:20:15 +0300

    Python variables; syntax and variable manipulation

---
+++ Lesson2(Variables).py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 31 17:55:03 2025
+
+@author: HP
+"""
+
+#Python Variables
+distance_travelled_mi = 100
+
+time_taken_min = 1200
+
+time_taken_hr = time_taken_min/60
+
+print("Number of miles traveled", distance_travelled_mi)
+print("Number of hours traveled", time_taken_hr)
+print("Speed in mph", distance_travelled_mi/time_taken_hr)
+
+print()
+
+#test
+#print("Time taken in minutes", Time_Taken_Min)
+# Answer: Will not work
+#Correct syntax
+print("Time taken in minutes", time_taken_min)
+
+#_total_memory = 0 should be
+ #total_memory = 01
+ 
+#_total% =25 should not use % symbol
+
+#this variable is correct
+totalPercentage =10
+
+#1stPass =3  should be and not start with a  number:
+firstPass = 3
+
+myName = "Faith Adhiambo"
+
+#should not use reserved words
+#class = "CIS 415"
+
+fruits = "Apples and Oranges"
+numApples=10
+numOranges=20
+print("I have", fruits, "...I have", numApples, "Apples", numOranges, "Oranges")
---
+++ test1.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 31 17:30:50 2025
+
+@author: Faith
+"""
+
+#Check whether the following statement will work or not
+#print(""Hi there!"")
+
+#Answer: None
+#Correct syntax
+
+print("Hi there!")
+
+print("." *20)
+#Or
+print("\"Hi there!\"")
+
+
